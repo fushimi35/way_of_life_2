9ff7304c572ceea41233959799bc2df05af18341
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-10-20 21:14:45 +0900

    Add: tailwindcssの適用

--- Gemfile
+++ Gemfile
@@ -63,3 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'net-smtp'
 gem 'net-imap'
 gem 'net-pop'
+
+#tailwind
+gem 'tailwindcss-rails'
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,10 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
+  end
+
+  def index2
+    @boards = Board.all
   end
 
   def show
